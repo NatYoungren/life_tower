92de8ee6bb3362f198bb32c0829f13759b8eeece
Nat Youngren <natahyoungren@gmail.com>
2023-08-18 18:19:43 -0400

    added working camera control

--- life_renderer_3d.py
+++ life_renderer_3d.py
@@ -23,7 +23,6 @@ def main():
     screen = pg.display.set_mode((SCREEN_W, SCREEN_H))
     running = True
     clock = pg.time.Clock()
-    
 
     surf = pg.surface.Surface((SCREEN_W, SCREEN_H))
     
@@ -34,15 +33,20 @@ def main():
     
     camera = np.array([13, 0.5, 2, 3.3, 0])
     # light_dir = np.array([0, 1, 1])
-
+    
     while running:
+        # print('start_pos', pg.mouse.get_pos())
+        # pg.mouse.set_pos((SCREEN_W/2, SCREEN_H/2))
+
+        elapsed_time = clock.tick(60) * 0.001
         
-        elapsed_time = clock.tick() * 0.001
+        surf.fill((50, 127, 200)) 
         
-        surf.fill((50, 127, 200))
+        # Define rotating light source
         light_dir = np.array([np.sin(pg.time.get_ticks()/1000), 1, 1])
         light_dir = light_dir/np.linalg.norm(light_dir)
-
+        # light_dir = camera[:3]/np.linalg.norm(camera[:3])
+        
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
@@ -66,9 +70,45 @@ def main():
             pg.draw.polygon(surf, color, triangle)
         
         screen.blit(surf, (0, 0))
+        
         pg.display.flip()
         pg.display.set_caption(str(round(1/(elapsed_time+1e-16), 2)) + ' fps : ' + str(camera) )
+        
+        move_camera(camera, elapsed_time)
 
+
+def move_camera(camera, elapsed_time):
+    
+    if pg.mouse.get_focused():
+        p_mouse = pg.mouse.get_rel()
+        if pg.mouse.get_pos() != (SCREEN_W/2, SCREEN_H/2):
+            pg.mouse.set_pos((SCREEN_W/2, SCREEN_H/2))
+        camera[3] = (camera[3] + 10 * elapsed_time * np.clip((p_mouse[0])/SCREEN_W, -0.2, 0.2)) % (2*np.pi)
+        camera[4] = camera[4] + 10 * elapsed_time * np.clip((p_mouse[1])/SCREEN_H, -0.2, 0.2)
+        camera[4] = np.clip(camera[4], -0.3, 0.3) #-np.pi/2, np.pi/2)
+
+    pressed_keys = pg.key.get_pressed()
+    
+    if pressed_keys[pg.K_w] or pressed_keys[pg.K_s] and pressed_keys[pg.K_a] or pressed_keys[pg.K_d]:
+        elapsed_time /= np.sqrt(2)
+    
+    
+    if pressed_keys[pg.K_w]:
+        camera[0] += 10 * elapsed_time * np.cos(camera[3])
+        camera[2] += 10 * elapsed_time * np.sin(camera[3])
+    elif pressed_keys[pg.K_s]:
+        camera[0] -= 10 * elapsed_time * np.cos(camera[3])
+        camera[2] -= 10 * elapsed_time * np.sin(camera[3])
+    
+    if pressed_keys[pg.K_a]:
+        camera[0] += 10 * elapsed_time * np.sin(camera[3])
+        camera[2] -= 10 * elapsed_time * np.cos(camera[3])
+    elif pressed_keys[pg.K_d]:
+        camera[0] -= 10 * elapsed_time * np.sin(camera[3])
+        camera[2] += 10 * elapsed_time * np.cos(camera[3])
+        
+    # pg.mouse.set_pos((SCREEN_W/2, SCREEN_H/2))
+    
 @njit()
 def sort_faces(vertices, faces, camera, z_order, light_dir, shade):
     for i, face in enumerate(faces):
@@ -86,7 +126,7 @@ def sort_faces(vertices, faces, camera, z_order, light_dir, shade):
         distance = np.sqrt(camera_ray[0]**2 + camera_ray[1]**2 + camera_ray[2]**2)
         camera_ray /= distance
         
-        if np.dot(normal, camera_ray) < 0:
+        if dot_3d(normal, camera_ray) < 0:
             z_order[i] = -distance
             shade[i] = dot_3d(light_dir, normal)/2 + 0.5
         else:
