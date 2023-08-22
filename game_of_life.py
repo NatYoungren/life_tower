348b70ba83c7e42dcedff76816e9761fb3c26614
Nat Youngren <natahyoungren@gmail.com>
2023-08-22 13:33:41 -0400

    Cleaned up and commented

--- game_of_life.py
+++ game_of_life.py
@@ -19,6 +19,7 @@ def fast_step(nmap, state, h, w):
 
 
 class GameOfLife():
+    # TODO: Deprecate radius and rules
     def __init__(self, width=10, height=10, radius=1, rules=None, initial_state=None, randomize_percent=0.3):
         self.width = width
         self.height = height
@@ -34,25 +35,27 @@ class GameOfLife():
             self.state = initial_state
         
         self.neighbor_map = np.zeros((height, width), dtype=int)
-        self.radius = radius
         
+        # NOTE: Not used by fast_step
+        self.radius = radius
         self.rules = rules if rules is not None else {True: {2: True, 3: True}, False: {3: True}}
     
-    def get_neighbors(self, i: int, j: int, radius=1):
-        # num_neighbors = 0
-        # for k in range(-radius, 1 + radius):
-        #     for l in range(-radius, 1 + radius):
-        #         if k == 0 and l == 0:
-        #             continue
-        #         elif i + k < 0 or i + k >= self.height:
-        #             continue
-        #         elif j + l < 0 or j + l >= self.width:
-        #             continue
-        #         elif self.state[i + k, j + l]:
-        #             num_neighbors += 1
-        
-        num_neighbors = np.sum(self.state[max(0, i - radius):i + radius + 1, max(0, j - radius):j + radius + 1]) - self.state[i, j]
+    def faster_but_uglier_get_neighbors(self, i: int, j: int, radius=1):
+        num_neighbors = 0
+        for k in range(-radius, 1 + radius):
+            for l in range(-radius, 1 + radius):
+                if k == 0 and l == 0:
+                    continue
+                elif i + k < 0 or i + k >= self.height:
+                    continue
+                elif j + l < 0 or j + l >= self.width:
+                    continue
+                elif self.state[i + k, j + l]:
+                    num_neighbors += 1
         return num_neighbors
+
+    def get_neighbors(self, i: int, j: int, radius=1):
+        return np.sum(self.state[max(0, i - radius):i + radius + 1, max(0, j - radius):j + radius + 1]) - self.state[i, j]
     
     def slow_step(self):
         new_state = np.zeros((self.height, self.width), dtype=np.bool_)
